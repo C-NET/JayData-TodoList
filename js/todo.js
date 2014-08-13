883974bd72041fe55d38891f8b53b62e42fc905f
Gustavo <github@c-net.com.ar>
2014-08-13 17:26:17 -0300

    Borrado de comentarios

--- js/todo.js
+++ js/todo.js
@@ -28,19 +28,7 @@ $(document).ready(function () {
         TodoEntries: { type: $data.EntitySet, elementType: $todo.Types.ToDoEntry }
     });
 
-    //$('#btnLoadContext').click(function () {
-    //    $todo.context = new $todo.Types.ToDoContext({ name: 'webSql', databaseName: 'todo' });
-    //    $todo.context.onReady({
-    //        success: updateView,
-    //        error: function () {
-    //            $todo.context = null;
-    //            updateView();
-    //        }
-    //    });
-    //});
-
     $('#btnAdd').click(function () {
-        $('#message').append("btnAdd");
         var value = $('#txtNew').val();
         if (!value) return;
         var now = new Date();
@@ -76,72 +64,7 @@ $(document).ready(function () {
 
 });
 
-
-//$(function () {
-//    $data.Entity.extend('$todo.Types.ToDoEntry', {
-//        Id: { type: 'int', key: true, computed: true },
-//        Value: { type: 'string' },
-//        CreatedAt: { type: 'datetime' },
-//        ModifiedAt: { type: 'datetime' },
-//        Done: { type: 'bool' }
-//    });
-
-//    $data.EntityContext.extend('$todo.Types.ToDoContext', {
-//        TodoEntries: { type: $data.EntitySet, elementType: $todo.Types.ToDoEntry }
-//    });
-
-//    $todo.context = new $todo.Types.ToDoContext({ name: 'webSql', databaseName: 'todo' });
-//    $todo.context.onReady({
-//        success: updateView,
-//        error: function () {
-//            $todo.context = null;
-//            updateView();
-//        }
-//    });
-
-//    $('#btnAdd').click(function () {
-//        //$('#todoList').append("AddButton");
-//        var value = $('#txtNew').val();
-//        if (!value) return;
-//        var now = new Date();
-//        //JayData code begins here
-//        var entity = new $todo.Types.ToDoEntry({ Value: value, CreatedAt: now, ModifiedAt: now });
-//        $todo.context.TodoEntries.add(entity);
-//        $todo.context.saveChanges(updateView);
-//    });
-
-//    //$('#linkAdd').click(function () {
-//    //    $('#todoList').append("AddLink");
-//    //});
-
-//    $('#btnClear').click(function () {
-//        $('#todoList > div').each(function () {
-//            var entity = $(this).data('entity');
-//            $todo.context.TodoEntries.remove(entity);
-//        });
-//        $todo.context.saveChanges(updateView);
-//    });
-
-//    $('#todoList').on('click', ':button', function (e) {
-//        var cmd = $(this).val();
-//        var entry = $(this).parent().data('entity');
-//        switch (cmd) {
-//            case 'undone':
-//            case 'done':
-//                $todo.context.TodoEntries.attach(entry);
-//                entry.Done = (cmd == 'done');
-//                break;
-//            case 'delete':
-//                $todo.context.TodoEntries.remove(entry);
-//                break;
-//        }
-//        $todo.context.saveChanges(updateView);
-//    });
-
-//});
-
 function updateView() {
-    $("#message").append("updateView");
     if ($todo.context) {
         $('#wrapper>div:not(#providerSelection)').show();
         $todo.context.TodoEntries.toArray(function (items) {
