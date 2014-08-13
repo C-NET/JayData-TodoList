932a5b12544d8472e8f707c8e9f1244886f65309
Gustavo <github@c-net.com.ar>
2014-08-13 15:39:58 -0300

    .

--- js/todo.js
+++ js/todo.js
@@ -1,65 +1,80 @@
-$(function () {
-    $data.Entity.extend('$todo.Types.ToDoEntry', {
-        Id: { type: 'int', key: true, computed: true },
-        Value: { type: 'string' },
-        CreatedAt: { type: 'datetime' },
-        ModifiedAt: { type: 'datetime' },
-        Done: { type: 'bool' }
-    });
+(function () {
+    document.addEventListener('deviceready', onDeviceReady, false);
+})();
 
-    $data.EntityContext.extend('$todo.Types.ToDoContext', {
-        TodoEntries: { type: $data.EntitySet, elementType: $todo.Types.ToDoEntry }
-    });
+function onDeviceReady() {
+    $("#todoList").append("ready");
+}
 
-    $todo.context = new $todo.Types.ToDoContext({ name: 'webSql', databaseName: 'todo' });
-    $todo.context.onReady({
-        success: updateView,
-        error: function () {
-            $todo.context = null;
-            updateView();
-        }
-    });
-
+$(document).ready(function () {
     $('#btnAdd').click(function () {
-        //$('#todoList').append("AddButton");
-        var value = $('#txtNew').val();
-        if (!value) return;
-        var now = new Date();
-        //JayData code begins here
-        var entity = new $todo.Types.ToDoEntry({ Value: value, CreatedAt: now, ModifiedAt: now });
-        $todo.context.TodoEntries.add(entity);
-        $todo.context.saveChanges(updateView);
+        $('#todoList').append("btnAdd");
     });
-
-    //$('#linkAdd').click(function () {
-    //    $('#todoList').append("AddLink");
-    //});
-
-    $('#btnClear').click(function () {
-        $('#todoList > div').each(function () {
-            var entity = $(this).data('entity');
-            $todo.context.TodoEntries.remove(entity);
-        });
-        $todo.context.saveChanges(updateView);
-    });
-
-    $('#todoList').on('click', ':button', function (e) {
-        var cmd = $(this).val();
-        var entry = $(this).parent().data('entity');
-        switch (cmd) {
-            case 'undone':
-            case 'done':
-                $todo.context.TodoEntries.attach(entry);
-                entry.Done = (cmd == 'done');
-                break;
-            case 'delete':
-                $todo.context.TodoEntries.remove(entry);
-                break;
-        }
-        $todo.context.saveChanges(updateView);
-    });
-
 });
+
+
+//$(function () {
+//    $data.Entity.extend('$todo.Types.ToDoEntry', {
+//        Id: { type: 'int', key: true, computed: true },
+//        Value: { type: 'string' },
+//        CreatedAt: { type: 'datetime' },
+//        ModifiedAt: { type: 'datetime' },
+//        Done: { type: 'bool' }
+//    });
+
+//    $data.EntityContext.extend('$todo.Types.ToDoContext', {
+//        TodoEntries: { type: $data.EntitySet, elementType: $todo.Types.ToDoEntry }
+//    });
+
+//    $todo.context = new $todo.Types.ToDoContext({ name: 'webSql', databaseName: 'todo' });
+//    $todo.context.onReady({
+//        success: updateView,
+//        error: function () {
+//            $todo.context = null;
+//            updateView();
+//        }
+//    });
+
+//    $('#btnAdd').click(function () {
+//        //$('#todoList').append("AddButton");
+//        var value = $('#txtNew').val();
+//        if (!value) return;
+//        var now = new Date();
+//        //JayData code begins here
+//        var entity = new $todo.Types.ToDoEntry({ Value: value, CreatedAt: now, ModifiedAt: now });
+//        $todo.context.TodoEntries.add(entity);
+//        $todo.context.saveChanges(updateView);
+//    });
+
+//    //$('#linkAdd').click(function () {
+//    //    $('#todoList').append("AddLink");
+//    //});
+
+//    $('#btnClear').click(function () {
+//        $('#todoList > div').each(function () {
+//            var entity = $(this).data('entity');
+//            $todo.context.TodoEntries.remove(entity);
+//        });
+//        $todo.context.saveChanges(updateView);
+//    });
+
+//    $('#todoList').on('click', ':button', function (e) {
+//        var cmd = $(this).val();
+//        var entry = $(this).parent().data('entity');
+//        switch (cmd) {
+//            case 'undone':
+//            case 'done':
+//                $todo.context.TodoEntries.attach(entry);
+//                entry.Done = (cmd == 'done');
+//                break;
+//            case 'delete':
+//                $todo.context.TodoEntries.remove(entry);
+//                break;
+//        }
+//        $todo.context.saveChanges(updateView);
+//    });
+
+//});
 
 function updateView() {
     if ($todo.context) {
