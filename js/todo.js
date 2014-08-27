d158563ae75c9ab3132e52a19e191295581bd5b7
Gustavo <github@c-net.com.ar>
2014-08-27 09:46:28 -0300

    cordova-plugin-network-information

--- js/todo.js
+++ js/todo.js
@@ -35,6 +35,28 @@ function onDeviceReady() {
 
     $("#sync").click(synchronizeData);
     $("#refresh").click(updateViewRemote);
+
+    checkConnection();
+
+
+}
+
+// Network info
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+    $("#networkinfo").append('Connection type: ' + states[networkState]);
 }
 
 $(document).ready(function () {
